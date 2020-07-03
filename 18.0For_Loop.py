f6afa1bacc44660aa42a4b2f49095cedc5eeb8e2
alnoor-tech <adamky2017@gmail.com>
2020-07-03 14:51:59 -0400

    EH1

---
+++ 18.0For_Loop.py
@@ -0,0 +1,10 @@
+#Helps us loop over different collections of items
+#Mostly used to loop through array, letters inside a string, or loop through series of numbers
+for letter in "I will make it alhamdulillah":
+    print(letter)
+for index in range(3):
+    if index == 0:
+        print("First itineration")
+    else:
+        print("Not first itineration")
+
---
+++ 18.1For loop.py
@@ -0,0 +1,13 @@
+cousins = ["Kianira", "Makena", "Kawira", "Munene", "Pria"]
+len(cousins) #This counts the nummber of elements in cousins array
+for index in range(len(cousins)):
+    print(cousins[index])
+for name in cousins:  #variable "name" can take any name
+    print(name + " loves me!")
+
+
+for index in range(10):
+    print(index)  #prints 0-9
+
+for index in range(3, 10):
+    print(index) #(prints 3-9)
